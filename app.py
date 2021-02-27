97cd54728f65e1df55bce1f98cace6feaa782d1d
TheLegendaryLuke <lukeueda730@gmail.com>
2021-02-27 17:44:14 +1100

    Basically, yeah

--- app.py
+++ app.py
@@ -4,39 +4,12 @@ from flask import request
 import json
 app = Flask(__name__)
 
-try:
-    with open("data.txt", "r") as f:
-        week = json.load(f)
-except:
-    week = {}
-    for d in range(7):
-        for s in range(48):
-            week["#D" + str(d) + f'{s:02}'] = "Free"
-    print(week)
-
-    a_file = open("data.txt", "w")
-
-    print(json.dumps(week))
-    a_file.write(json.dumps(week))
-    a_file.close()
 
 @app.route("/home")
 @app.route("/")
 def hello():
     print("Hello")
-    return render_template('home.html', data=week)
-
-@app.route("/press/", methods=["POST"])
-def press(methods=["POST"]):
-    cell = request.data.decode("utf-8")
-    week[cell[8: 13]] = cell[13:-2]
-
-    a_file = open("data.txt", "w")
-
-    a_file.write(json.dumps(week))
-    a_file.close()    
-
-    return {"":""}
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
