1b327210638a5c64e2aa0a34ae9ea4500799f066
TheLegendaryLuke <47493957+TheLegendaryLuke@users.noreply.github.com>
2021-04-30 18:11:00 +1000

    Structure Change + updates
    - Main class implemented
    - Cells can no longer be pressed through UI
    - Button function structure

--- static/js/createCalender.js
+++ static/js/createCalender.js
@@ -1,8 +1,11 @@
 class Day {
     constructor(day){
+
+        this.value = day
+
         this.container = document.createElement("div");
         this.container.setAttribute("class", "day");
-        this.container.setAttribute("id", day);
+        this.container.setAttribute("id", this.value);
 
         this.cells = []
         this.createCells(96);
@@ -28,6 +31,9 @@ class Cell{
         this.container.setAttribute("class", "cell");
         this.container.style.backgroundColor = "#FFFFFF";
 
+        this.container.setAttribute("data-d", day.value);
+        this.container.setAttribute("data-p", number);
+
         //Asign values
         var text = document.createElement("div");
         text.style.position = "relative";
@@ -39,33 +45,40 @@ class Cell{
         var container = this.container;
 
         this.container.addEventListener("mousedown", function(){
-            //If highlight mode is active, than the pressed cell is the end of the segment.
-            if(document.getElementById("mode").getAttribute("class") == "highlight"){
-                //Exit highlight mode.
-                document.getElementById("mode").setAttribute("class", "");
+            var mode = document.getElementById("mode")
+            
+            //Only able to be pressed if overlay UI is not active.
+            if(mode.getAttribute("class") != "ui"){
 
-                //Style change;
-                container.style.border = "2px solid black";
-                container.style.borderTop = "";
-                text.innerHTML = "<" + String((number + 1) * 0.25);
+                //If highlight mode is active, than the pressed cell is the end of the segment.
+                if(mode.getAttribute("class") == "highlight"){
 
-                //Set value for highlight end.
-                day.highlightEnd = (number + 1) * 0.25;
-                console.log(day.highlightStart + "," + day.highlightEnd);
+                    //Exit highlight mode and disable cell highlight.
+                    mode.setAttribute("class", "ui");
 
-                //Activate task set menu.
-                document.getElementById("highlight_ui").style.display = "block";
-            }
+                    //Style change;
+                    container.style.border = "2px solid black";
+                    container.style.borderTop = "";
+                    text.innerHTML = "<" + String((number + 1) * 0.25);
 
-            //If highlight mode is not active, than activate and set current cell to begining.
-            else if(document.getElementById("mode").getAttribute("class") != "highlight"){
-                document.getElementById("mode").setAttribute("class", "highlight");
-                day.highlightStart = number * 0.25;
+                    //Set value for highlight end.
+                    day.highlightEnd = (number + 1) * 0.25;
+                    console.log(day.highlightStart + "," + day.highlightEnd);
 
-                text.innerHTML = "<" + String(number * 0.25);
+                    //Activate task set menu.
+                    document.getElementById("highlight_ui").style.display = "block";
+                }
 
-                container.style.border = "2px solid black";
-                container.style.borderBottom = "";
+                //If highlight mode is not active, than activate and set current cell to begining.
+                else if(mode.getAttribute("class") != "highlight"){
+                    mode.setAttribute("class", "highlight");
+                    day.highlightStart = number * 0.25;
+
+                    text.innerHTML = "<" + String(number * 0.25);
+
+                    container.style.border = "2px solid black";
+                    container.style.borderBottom = "";
+                }
             }
         });
     }
@@ -111,8 +124,7 @@ class WeekContainer {
 
 class Menu{
     constructor(){
-        this.container = document.createElement("div");
-        this.container.setAttribute("class", "menu");
+        this.container = document.getElementById("menu");
     }
 }
 
@@ -127,11 +139,40 @@ class WeekTitle{
     }
 }
 
-w = new WeekNavigationContainer();
-m = new Menu();
-t = new WeekTitle();
-t.setTitle("Yeet");
+class HighlightUI{
+    constructor(){
+        var clear = document.getElementById("clear");
+        var addTask = document.getElementById("task");
 
-document.querySelector(".main").appendChild(m.container);
-document.querySelector(".main").appendChild(t.container);
-document.querySelector(".main").appendChild(w.container);
+        clear.addEventListener("mousedown", clearFunc);
+        addTask.addEventListener("mousedown", addTaskFunc);
+    }
+}
+
+class Main{
+    constructor(){
+        var w = new WeekNavigationContainer();
+        var m = new Menu();
+        var t = new WeekTitle();
+        var h = new HighlightUI();
+        t.setTitle("Yeet");
+
+        var body = document.querySelector(".main");
+
+        body.appendChild(m.container);
+        body.appendChild(t.container);
+        body.appendChild(w.container);
+    }
+}
+
+function clearFunc(){
+    document.getElementById("highlight_ui").style.display = "none";
+    document.getElementById("mode").setAttribute("class", "");
+}
+
+function addTaskFunc(){
+    document.getElementById("highlight_ui").style.display = "none";
+    document.getElementById("mode").setAttribute("class", "");
+}
+
+main = new Main();
