d0a03dd97300b5f9cf3d58abfb137144bd933353
TheLegendaryLuke <47493957+TheLegendaryLuke@users.noreply.github.com>
2021-05-18 22:01:43 +1000

    Progress

--- client/src/components/helpButton.js
+++ client/src/components/helpButton.js
@@ -4,7 +4,10 @@ import color from './images/Colors.png'
 import findingFriends from './images/FindingFriends.png'
 import fortnite from './images/Fortnite.png'
 import addTask from './images/AddTask.png'
+import editMenu from './images/EditMenu.png'
+import editTask from './images/EditMode.png'
 import addHeader from './images/AddHeader.png'
+import prevNext from './images/PrevNext.png'
 
 function HelpButton(){
     const target = useRef(null);
@@ -25,44 +28,56 @@ function HelpButton(){
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <h2><u>Navigation:</u></h2>
+                    Upon launching the software, the current week will be displayed in the UI.
+                    In order to navigate throughout the year, the <em>prev</em> and <em>next</em> buttons allow you
+                    to cycle through the weeks.
+                    <br/>
+                    <Image src={prevNext} fluid  className="border border-dark"/>
+                    <br/><br/><br/>
                     <h2><u>Adding Tasks:</u></h2>
                     Add mode allows you to create tasks.
                     First, the interface must be in add mode,
                     activated through the radio button located on the top left and indicated by the blue header.
                     <br/>
                     <Image src={addHeader} fluid width='30%' height='30%' className="border border-dark"/>
-                    <br/>
-                    <br/>
+                    <br/><br/>
                     Once in Add Mode, a period of time can be selected through pressing on the appropriate cells.
                     Upon hovering over a cell, a pop-over will show what block of time it represents.
                     Don't worry if you miss click, start and end times can be changed in the task creation menu.
                     <br/>
                     <Image src={addTask} fluid width='40%' height='40%' className="border border-dark"/>
-                    <br/>
-                    <br/>
+                    <br/><br/>
                     Within the menu, your selected values appear in the start and end fields, as well as an entry for the task name.
                     Here you can change your prior selection and enter in your desired task name.
                     Currently, there is not support for custom colors, however there are preset colors for specified names:
                     <Image src={color} fluid width='70%' height='70%' className="border border-dark"/>
-                    <br/>
-                    <br/>
+                    <br/><br/>
                     ...Otherwise, any custom names will appear in a grey coloring.
                     <br/>
                     <Image src={fortnite} fluid width='40%' height='40%' className="border border-dark"/>
-                    <br/>
-                    <br/>
-                    Note: If you press cells that intersect with other tasks, the other tasks will be altered or removed so that the task can be submitted
+                    <br/><br/>
+                    Note: If you press cells that intersect with other tasks, the other tasks will be altered or removed so that the task can be submitted.
                     <br/>
                     <Image src={findingFriends} fluid width='70%' height='70%' className="border border-dark"/>
-                    <br/>
-                    <br/>
+                    <br/><br/><br/>
                     <h2><u>Editing Tasks:</u></h2>
-                    Edit mode allows you to alter added tasks. In edit mode, pressing tasks activates the edit menu, which is essentially a modified
+                    Edit mode allows you to alter added tasks. First, the interface must be in edit mode,
+                    activated through the radio button located on the top left and indicated by the yellow header.
+                    <br/>
+                    <Image src={editTask} fluid width='30%' height='30%' className="border border-dark"/>
+                    <br/><br/>
+                    In edit mode, pressing tasks activates the edit menu, which is essentially a modified
                     <em> Add Mode </em>
-                    UI. Upon submission the current task will be replaced by one with the modified parameters. 
+                    UI.
+                    <br/>
+                    <Image src={editMenu} fluid width='40%' height='40%' className="border border-dark"/>
+                    <br/><br/>
+                    Upon submission the current task will be replaced by one with the modified parameters.
+                    Intersecting cells will be adjusted just as they did for adding tasks. I think the delete button is self explanatory.
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant='primary' closeButton onClick={togglePop}>Okay</Button>
+                    <Button variant='primary' closeButton onClick={togglePop}>Close</Button>
                 </Modal.Footer>
             </Modal>
         </div>
